8fa7fb430f08eadf8b5ae1c87314754f1d3a832c
Tyler Hunstock <Thunstock17@gmail.com>
2019-04-08 16:36:09 -0700

    Cookies

--- app.js
+++ app.js
@@ -40,38 +40,49 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
-// function auth(req, res, next) {
-// 	console.log(req.headers);
+function auth(req, res, next) {
+	console.log(req.signedCookies);
 
-// 	let authHeader = req.headers.authorization;
+	if (!req.signedCookies.user) {
+		let authHeader = req.headers.authorization;
 
-// 	if (!authHeader) {
-// 		let err = new Error('You are not authenticated!');
+		if (!authHeader) {
+			let err = new Error('You are not authenticated!');
 
-// 		res.setHeader('WWW-Authenticate', 'Basic');
-// 		err.status = 401;
-// 		return next(err);
-// 	}
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
 
-// 	let auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+		let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
 
-// 	let username = auth[0];
-// 	let password = auth[1];
+		let username = auth[0];
+		let password = auth[1];
 
-// 	if (username === 'admin' && password === 'password') {
-// 		next();
-// 	} else {
-// 		let err = new Error('You are not authenticated!');
+		if (username === 'admin' && password === 'password') {
+			res.cookie('user', 'admin', { signed: true });
+			next();
+		} else {
+			let err = new Error('You are not authenticated!');
 
-// 		res.setHeader('WWW-Authenticate', 'Basic');
-// 		err.status = 401;
-// 		return next(err);
-// 	}
-// }
+			res.setHeader('WWW-Authenticate', 'Basic');
+			err.status = 401;
+			return next(err);
+		}
+	} else {
+		if (req.signedCookies.user === 'admin') {
+			next();
+		} else {
+			let err = new Error('You are not authenticated!');
+			err.status = 401;
+			return next(err);
+		}
+	}
+}
 
-// app.use(auth);
+app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
